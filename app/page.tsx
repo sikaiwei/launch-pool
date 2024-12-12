91fa5edc47b0a5efa464662ada55a9cf28ac9c9f
sikaiwei <kaiwei_si@hmglog.com>
2024-12-12 23:43:04 +0800

    add appreance

--- app/page.tsx
+++ app/page.tsx
@@ -161,6 +161,84 @@ export default function Home() {
         Amet commodo nisi quis irure velit Lorem enim anim commodo sunt aliquip
         officia quis.
       </div>
+      <div class="w-96 m-5 p-5 border-4 rounded-xl">
+        Dolore deserunt sunt qui ut quis sunt anim do nostrud minim fugiat
+        minim.
+      </div>
+
+      <button class="m-5 outline outline-4">Click</button>
+
+      <div class="blur-sm">
+        Do elit adipisicing cupidatat dolor excepteur nulla in incididunt.
+      </div>
+
+      <div class="h-16 bg-red-500 brightness-50">Brightness 0</div>
+      <div class="h-16 bg-red-500 brightness-100">Brightness 100</div>
+      <div class="h-16 bg-red-500 brightness-200">Brightness 200</div>
+
+      <div class="h-16 bg-red-500 contrast-0">Contrast 0</div>
+      <div class="h-16 bg-red-500 contrast-50">Contrast 50</div>
+      <div class="h-16 bg-red-500 contrast-100">Contrast 200</div>
+      <div class="h-16 bg-red-500 contrast-200">Contrast 200</div>
+
+      <div class="h-16 bg-red-500 grayscale">Grayscale</div>
+
+      <div class="h-16 bg-red-500 invert">Invert</div>
+      <div class="h-16 bg-red-500 sepia">Sepia</div>
+
+      <div class="h-16 bg-red-500 hue-rotate-0">Hue Rotate 0</div>
+      <div class="h-16 bg-red-500 hue-rotate-90">Hue Rotate 90</div>
+      <div class="h-16 bg-red-500 hue-rotate-180">Hue Rotate 180</div>
+
+      <button
+        class="bg-black text-white rounded-lg m-5 p-5
+       hover:bg-blue-200 
+       hover:text-black"
+      >
+        Click
+      </button>
+
+      <button
+        class="bg-black text-white rounded-lg m-5 p-5
+       focus:bg-blue-200 focus:text-black"
+      >
+        Click
+      </button>
+
+      <button
+        class="bg-black text-white rounded-lg m-5 p-5
+       active:bg-blue-200 active:text-black"
+      >
+        Click
+      </button>
+
+      <div
+        class="group block max-w-xs mx-auto rounded-lg p-6 bg-white shadow-lg space-y-3 
+      hover:bg-red-500"
+      >
+        <h3 class="hover:text-white">Title</h3>
+        <p class="group-hover:text-white">
+          Laboris tempor ex nisi deserunt labore anim et do in officia sint
+          laborum.
+        </p>
+      </div>
+
+      <ul>
+        <li class="first:bg-red-500">Item 1</li>
+        <li class="first:bg-red-500">Item 2</li>
+        <li class="first:bg-red-500 even:bg-red-200 odd:bg-red-700">Item 3</li>
+        <li class="first:bg-red-500 even:bg-red-200 odd:bg-red-700">Item 4</li>
+        <li class="first:bg-red-500 even:bg-red-200 odd:bg-red-700">Item 5</li>
+      </ul>
+
+      <select class="border-4 border-red-500">
+        <option value="1">True</option>
+        <option value="0">False</option>
+      </select>
+      <select class="border-4 border-green-500 appearance-none">
+        <option value="1">True</option>
+        <option value="0">False</option>
+      </select>
     </div>
   );
 }

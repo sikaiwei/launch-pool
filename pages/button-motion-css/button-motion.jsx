90987bcb370a9a648816ca0616be0c6b19f62455
sikaiwei <kaiwei_si@hmglog.com>
2024-12-18 23:20:55 +0800

    grid 定位元素

--- pages/button-motion-css/button-motion.jsx
+++ pages/button-motion-css/button-motion.jsx
@@ -1,15 +1,20 @@
 "use client";
 import style from "./styles.module.css";
-
-
-
-
+import { motion } from "motion/react";
 
 export default function BtnMotionCss() {
   return (
     /* From Uiverse.io by MuhammadHasann */
-    <div className="bg-black p-12 w-2/6">
-      <button className={style.button}>边框按钮</button>
+    <div className="grid grid-cols-3 gap-3 ">
+      {/* <div className="bg-black p-12 w-2/6"> */}
+      <div></div>
+      <motion.button
+        whileHover={{ scale: 1.03 }}
+        className={`${style.button} place-self-end col-start-2 col-span-2`}
+      >
+        边框按钮
+      </motion.button>
+      {/* </div> */}
     </div>
   );
 }

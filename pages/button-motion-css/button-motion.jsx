7589841c6c80cd9324691ce80fb8c75f81c39acc
sikaiwei <sikaiwei0@163.com>
2024-12-19 10:23:03 +0800

    add

--- pages/button-motion-css/button-motion.jsx
+++ pages/button-motion-css/button-motion.jsx
@@ -10,6 +10,7 @@ export default function BtnMotionCss() {
     /* From Uiverse.io by MuhammadHasann */
     <div className="bg-black p-12 w-2/6">
       <button className={style.button}>边框按钮</button>
+      <div className={style.button}>盒子</div>
     </div>
   );
 }
